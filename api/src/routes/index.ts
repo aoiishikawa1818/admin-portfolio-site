247b862f28a5ae9c75b4a68db2be2143fc822ec7
aoiishikawa <aoiishikawa1818@gmail.com>
2026-03-13 07:07:49 +0900

    fix: 管理APIにFirebase認証を追加する

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -9,6 +9,7 @@ import {
   putAdminSkill,
 } from "../controllers/adminSkillController";
 import { postContact } from "../controllers/contactController";
+import { verifyFirebaseToken } from "../middlewares/verifyFirebaseToken";
 import { getHealth } from "../controllers/healthController";
 import { getPortfolio } from "../controllers/portfolioController";
 
@@ -17,10 +18,10 @@ const router = Router();
 router.get("/health", getHealth);
 router.get("/portfolio", getPortfolio);
 router.post("/contacts", postContact);
-router.put("/admin/profile", putAdminProfile);
-router.post("/admin/works", postAdminWork);
-router.put("/admin/works/:id", putAdminWork);
-router.post("/admin/skills", postAdminSkill);
-router.put("/admin/skills/:id", putAdminSkill);
+router.put("/admin/profile", verifyFirebaseToken, putAdminProfile);
+router.post("/admin/works", verifyFirebaseToken, postAdminWork);
+router.put("/admin/works/:id", verifyFirebaseToken, putAdminWork);
+router.post("/admin/skills", verifyFirebaseToken, postAdminSkill);
+router.put("/admin/skills/:id", verifyFirebaseToken, putAdminSkill);
 
 export default router;
